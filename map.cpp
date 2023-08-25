4ba0d8b826e154dd7f747c26a71124c24c1f6c5b
szb1551 <2262400256@qq.com>
2023-08-25 21:03:16 +0800

    添加了图片变换

--- map.cpp
+++ map.cpp
@@ -8,6 +8,13 @@ Draw::Draw(HBITMAP hBit, HWND hw)
 	hBitmap = hBit;
 	hwnd = hw;
 }
+Draw::Draw(HBITMAP hBit, HWND hw, int width, int height)
+{
+	hBitmap = hBit;
+	hwnd = hw;
+	NowWidth = width;
+	NowHeight = height;
+}
 void Draw::DrawBackground()
 {
 	PAINTSTRUCT ps;
@@ -19,7 +26,7 @@ void Draw::DrawBackground()
 	//读取图像
 	if (GetImage())
 	{
-		StretchBlt(hdc, 0, 0, WindowWidth, WindowHeight, hdcMem, 0, 0, ImageWidth, ImageHeight, SRCCOPY);
+		StretchBlt(hdc, 0, 0, NowWidth, NowHeight, hdcMem, 0, 0, ImageWidth, ImageHeight, SRCCOPY);
 	}
 	//绘制图像
 	//BitBlt(hdc, 0, 0, 1284, 588, hdcMem, 0, 0, SRCCOPY);
@@ -48,6 +55,19 @@ long long WINAPI WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 {
 	switch (msg)
 	{
+		case WM_SIZE:
+		{
+			//当窗口大小发生改变时，更新图片的大小和位置
+			RECT rc;
+			GetWindowRect(hwnd, &rc);
+			int width = rc.right - rc.left;
+			int height = rc.bottom - rc.top;
+			Draw draw(hBitmap, hwnd, width, height);
+			draw.DrawBackground();
+			draw.~Draw();
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		}
 		case WM_PAINT:
 		{
 			Draw draw(hBitmap, hwnd);
@@ -64,8 +84,12 @@ long long WINAPI WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 		}
 		case WM_CLOSE:
 		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+			break;
+		}
 		case WM_QUIT:
-			exit(0); 
+			exit(0);
 		break;
 	}
 	return DefWindowProc(hwnd, msg, wp, lp);
@@ -83,6 +107,12 @@ HWND set_window(HINSTANCE hInst)
 	HWND hwnd = CreateWindowW(L"WindowClassName", L"你好", WS_VISIBLE | WS_OVERLAPPEDWINDOW, 100, 100, WindowWidth, WindowHeight, NULL, NULL, hInst, NULL);
 	if (hwnd == NULL) return (HWND) - 1;
 	hBitmap = (HBITMAP)LoadImage(NULL, L"image/mypaper_title.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+
+	HWND hButton = CreateWindow(L"Button", L"开始游戏", WS_CHILD | WS_VISIBLE, 100, 50, 100, 30, hwnd, (HMENU)1, NULL, NULL);
+
+	ShowWindow(hwnd, SW_SHOWDEFAULT);
+	UpdateWindow(hwnd);
+
 	MSG msg;
 	while (true) if (GetMessage(&msg, NULL, 0, 0))
 	{
--- map.h
+++ map.h
@@ -29,31 +29,13 @@ private:
 	HDC hdc;
 	int ImageWidth;
 	int ImageHeight;
+	int NowWidth=WindowWidth;
+	int NowHeight = WindowHeight;
 public:
-	Draw(HBITMAP hBit, HWND hw, char* ImageFile);
+	Draw(HBITMAP hBit, HWND hw, int width, int height);
 	Draw(HBITMAP hBit, HWND hw);
 	void DrawBackground();
 	bool GetImage();
 	~Draw();
-};
-
-// BMP 文件头结构
-struct BMPHeader {
-    uint16_t fileType;       // 文件类型标识（"BM"）
-    uint32_t fileSize;       // 文件大小
-    uint16_t reserved1;      // 保留字段1
-    uint16_t reserved2;      // 保留字段2
-    uint32_t offset;         // 位图数据的偏移量
-    uint32_t headerSize;     // 文件头大小
-    int32_t width;           // 图像宽度
-    int32_t height;          // 图像高度
-    uint16_t planes;         // 颜色平面数
-    uint16_t bitsPerPixel;   // 每像素位数
-    uint32_t compression;    // 压缩方式
-    uint32_t imageSize;      // 图像数据大小
-    int32_t xPixelsPerMeter; // 水平分辨率
-    int32_t yPixelsPerMeter; // 垂直分辨率
-    uint32_t colorsUsed;     // 使用的颜色数
-    uint32_t colorsImportant; // 重要的颜色数
 };
 
